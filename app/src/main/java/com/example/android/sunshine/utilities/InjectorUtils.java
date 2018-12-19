466a53703e5fd0b50edb770dfc610780b85f1e25
Ruslan <ruslansolodovnyk@gmail.com>
2018-12-19 09:46:01 +0200

    finish step 16

--- app/src/main/java/com/example/android/sunshine/utilities/InjectorUtils.java
+++ app/src/main/java/com/example/android/sunshine/utilities/InjectorUtils.java
@@ -19,29 +19,45 @@ package com.example.android.sunshine.utilities;
 /**
  * Provides static methods to inject the various classes needed for Sunshine
  */
+import android.content.Context;
+
+import com.example.android.sunshine.AppExecutors;
+import com.example.android.sunshine.data.SunshineRepository;
+import com.example.android.sunshine.data.database.SunshineDatabase;
+import com.example.android.sunshine.data.network.WeatherNetworkDataSource;
+import com.example.android.sunshine.ui.detail.DetailViewModelFactory;
+import com.example.android.sunshine.ui.list.MainViewModelFactory;
+
+import java.util.Date;
+
+/**
+ * Provides static methods to inject the various classes needed for Sunshine
+ */
 public class InjectorUtils {
 
-//    public static SunshineRepository provideRepository(Context context) {
-//        SunshineDatabase database = SunshineDatabase.getInstance(context.getApplicationContext());
-//        AppExecutors executors = AppExecutors.getInstance();
-//        WeatherNetworkDataSource networkDataSource =
-//                WeatherNetworkDataSource.getInstance(context.getApplicationContext(), executors);
-//        return SunshineRepository.getInstance(database.weatherDao(), networkDataSource, executors);
-//    }
-//
-//    public static WeatherNetworkDataSource provideNetworkDataSource(Context context) {
-//        AppExecutors executors = AppExecutors.getInstance();
-//        return WeatherNetworkDataSource.getInstance(context.getApplicationContext(), executors);
-//    }
-//
-//    public static DetailViewModelFactory provideDetailViewModelFactory(Context context, Date date) {
-//        SunshineRepository repository = provideRepository(context.getApplicationContext());
-//        return new DetailViewModelFactory(repository, date);
-//    }
-//
-//    public static MainViewModelFactory provideMainActivityViewModelFactory(Context context) {
-//        SunshineRepository repository = provideRepository(context.getApplicationContext());
-//        return new MainViewModelFactory(repository);
-//    }
+    public static SunshineRepository provideRepository(Context context) {
+        SunshineDatabase database = SunshineDatabase.getInstance(context.getApplicationContext());
+        AppExecutors executors = AppExecutors.getInstance();
+        WeatherNetworkDataSource networkDataSource =
+                WeatherNetworkDataSource.getInstance(context.getApplicationContext(), executors);
+        return SunshineRepository.getInstance(database.weatherDao(), networkDataSource, executors);
+    }
 
+    public static WeatherNetworkDataSource provideNetworkDataSource(Context context) {
+        // This call to provide repository is necessary if the app starts from a service - in this
+        // case the repository will not exist unless it is specifically created.
+        provideRepository(context.getApplicationContext());
+        AppExecutors executors = AppExecutors.getInstance();
+        return WeatherNetworkDataSource.getInstance(context.getApplicationContext(), executors);
+    }
+
+    public static DetailViewModelFactory provideDetailViewModelFactory(Context context, Date date) {
+        SunshineRepository repository = provideRepository(context.getApplicationContext());
+        return new DetailViewModelFactory(repository, date);
+    }
+
+    public static MainViewModelFactory provideMainActivityViewModelFactory(Context context) {
+        SunshineRepository repository = provideRepository(context.getApplicationContext());
+        return new MainViewModelFactory(repository);
+    }
 }
